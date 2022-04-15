877930e97c3fc95cec073a451804d12856b98613
CeyranSultanova <ceyransultanova11@gmail.com>
2022-04-15 20:26:54 +0400

    test6

---
+++ test 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace test_6
+{
+    class Program
+    {
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                if (value > 0)
+                { number = 6; }
+                else 
+                { number = 0; }
+
+            }
+        }
+        public override string ToString()
+        {
+            return $"{number}";
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Program test = new Program();
+            test.number = -9;
+            Console.WriteLine(test);
+        }
+
+
+
+
+
+
+
+
+    }
+}
